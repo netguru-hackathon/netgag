96aaa18dfd2875db76d19a93cf62079dae55fdc9
Jakub Kubacki <kubacki.jk@gmail.com>
2016-06-24 12:04:56 +0200

    Fix typos

--- config/prod.exs
+++ config/prod.exs
@@ -14,14 +14,14 @@ use Mix.Config
 config :netgag, Netgag.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "netgag.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 
 # Configure your database
 config :netgag, Netgag.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
